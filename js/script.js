c0ee653b33a4fa66c9b893ce6a07c27a31d7f5e0
Juan Lee <juanlee@kaist.ac.kr>
2021-08-13 23:17:12 +0900

    Complete 1st of mobile

--- js/script.js
+++ js/script.js
@@ -3,11 +3,17 @@
 this.window.addEventListener("scroll", function (e) {
     const offsetY = this.window.scrollY;
     const height = this.document.body.clientHeight;
-    const navbarHeight = 85 / 2; // divide for smooth change
+    const width = this.document.body.clientWidth;
+
+    let navbarHeight = 85 / 2; // divide for smooth change
+    if (width <= 800) {
+        navbarHeight = 56 / 2;
+    }
 
     let navbar = this.document.querySelector("nav");
     let logo = this.document.querySelector(".logo");
     let menus = this.document.querySelectorAll(".menu");
+    let menuBtn = this.document.querySelector("nav .menu-btn > img");
 
     const offsetHome = 0;
     const offsetAlliance = this.document.getElementById("alliance").getBoundingClientRect().top + offsetY;
@@ -18,12 +24,14 @@ this.window.addEventListener("scroll", function (e) {
     if ((offsetY >= navOffsetHome && offsetY <= navOffsetHome + height) || (offsetY >= navOffsetAlliance && offsetY <= navOffsetAlliance + height)) {
         navbar.style.backgroundColor = "transparent";
         logo.src = "./assets/images/logow@3x.png";
+        menuBtn.src = "./assets/icons/menuw.svg";
         menus.forEach(function (item) {
             item.style.color = "white";
         });
     } else {
         navbar.style.backgroundColor = "white";
         logo.src = "./assets/images/logob@3x.png";
+        menuBtn.src = "./assets/icons/menub.svg";
         menus.forEach(function (item) {
             item.style.color = "black";
         });
@@ -61,3 +69,13 @@ $(document).ready(() => {
         },
     });
 });
+
+/* side bar */
+
+function openNav() {
+    document.querySelector("aside").style.width = "300px";
+}
+
+function closeNav() {
+    document.querySelector("aside").style.width = "0";
+}
