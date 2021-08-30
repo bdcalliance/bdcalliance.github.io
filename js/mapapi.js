10f9d2cb5c2ff4f115f679119ce45f8dc349cce9
Juan Lee <juanlee@kaist.ac.kr>
2021-08-31 01:49:18 +0900

    naver point

--- js/mapapi.js
+++ js/mapapi.js
@@ -2,3 +2,8 @@ var map = new naver.maps.Map("map", {
     center: new naver.maps.LatLng(37.5092693, 127.0523556),
     zoom: 18,
 });
+
+var marker = new naver.maps.Marker({
+    position: new naver.maps.LatLng(37.5092693, 127.0523556),
+    map: map,
+});
