784925d38e3fc86a0ec6ec37a41f6e21cb9fdd72
Juan Lee <juanlee@kaist.ac.kr>
2021-08-31 01:23:45 +0900

    naver point

--- js/mapapi.js
+++ js/mapapi.js
@@ -1,6 +1,8 @@
 let naverPt = new naver.maps.Point(14143403.529477883, 4510332.097012131);
 let latlng = naver.maps.TransCoord.fromNaverToLatLng(naverPt);
 
+console.log(latlng.toString());
+
 var mapOptions = {
     center: latlng,
     zoom: 20,
