64e9f1f98d1aa3f11711186f55324d04307527b1
Juan Lee <juanlee@kaist.ac.kr>
2021-08-31 00:56:10 +0900

    32z

--- js/mapapi.js
+++ js/mapapi.js
@@ -1,6 +1,6 @@
 var mapOptions = {
-    center: new naver.maps.LatLng(37.5092314, 127.0501384),
-    zoom: 32,
+    center: new naver.maps.NAVER(14143403.529477883, 4510332.097012131),
+    zoom: 17,
 };
 
 var map = new naver.maps.Map("map", mapOptions);
