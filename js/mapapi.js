d5ad0c66953fe396d8cc43e18af8031de1bddc33
Juan Lee <juanlee@kaist.ac.kr>
2021-08-31 01:33:55 +0900

    naver point

--- js/mapapi.js
+++ js/mapapi.js
@@ -1,11 +1,21 @@
-let naverPt = new naver.maps.Point(14143403.529477883, 4510332.097012131);
-let latlng = naver.maps.TransCoord.fromNaverToLatLng(naverPt);
+naver.maps.Service.geocode(
+    {
+        query: "서울특별시 강남구 봉은사로68길 49",
+    },
+    function (status, response) {
+        if (status !== naver.maps.Service.Status.OK) {
+            return alert("Something wrong!");
+        }
 
-console.log(latlng.toString());
+        var x = response.v2.addresses[0].x;
+        var y = response.v2.addresses[0].y;
 
-var mapOptions = {
-    center: latlng,
-    zoom: 20,
-};
+        console.log(x);
+        console.log(y);
 
-var map = new naver.maps.Map("map", mapOptions);
+        var map = new naver.maps.Map("map", {
+            center: new naver.maps.LatLng(x, y),
+            zoom: 20,
+        });
+    }
+);
